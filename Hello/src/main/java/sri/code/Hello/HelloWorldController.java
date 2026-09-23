08d5d4d5201570d49e48b351b4d1189c306e19a1
srinarmatha123 <lsrinarmatha@gmail.com>
2026-09-23 12:43:17 +0530

    Fix CORS for deployed frontend

--- Hello/src/main/java/sri/code/Hello/HelloWorldController.java
+++ Hello/src/main/java/sri/code/Hello/HelloWorldController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://todo-frontend-fp68.onrender.com"
+})
 @RequestMapping("/tasks")
 public class HelloWorldController {
 
